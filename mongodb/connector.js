100c88c9f7fe4c32f1c6fada33e4e6fbea284ba9
Gabriel Henrique <gabriel.holiveira@live.com>
2020-04-04 03:43:03 -0300

    config

--- mongodb/connector.js
+++ mongodb/connector.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
-const uri = `mongodb://localhost:27017`
+const config = require("../config/index.json")
+const uri = config.mongodb.uri
 const client = new MongoClient(uri, { useNewUrlParser: true })
 const conn = client.connect()
 module.exports = conn
